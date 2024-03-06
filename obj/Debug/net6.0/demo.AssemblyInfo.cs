65adc1c918fe2e088e9d24c851eeff9c8b433217
Ahmed <ahmeed02003@gmail.com>
2024-03-06 14:50:15 +0200

    last edit

--- obj/Debug/net6.0/demo.AssemblyInfo.cs
+++ obj/Debug/net6.0/demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a554637b4f1f359fbd9f6554231fe87f3bb90a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
